21493d15f10401707003dbdf54212d361359089e
Vincent Dao <vincentdao288@gmail.com>
2025-08-20 21:19:03 +1000

    Commiting, continuing tomorrow.

---
+++ main.js
@@ -0,0 +1,23 @@
+const submitBtn = document.getElementsByClassName("submitBtn")[0];
+
+function handleErrorState(){
+
+}
+function handleRequired(){
+  /* 
+
+    Check if firstName, lastName, email, queryType, message and isConsentGiven filled out
+
+  */
+}
+
+submitBtn.addEventListener("submit", (e) => {
+  /*
+
+    Handle error-handling top to bottom
+      a. Check if required field is missing
+      b. Check if the email address is not formatted correctly
+
+    1. handleRequired() to check if the inputs are empty or not
+  */
+});
